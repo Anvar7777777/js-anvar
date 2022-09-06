2e9fb41fbdaeb7e6de120f812d52e89b40138a95
Anvar7777777 <toshpolatovanvar777@gmail.com>
2022-09-06 18:46:11 +0500

    btn push

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+alert ("Welcome");
+
+
+
+var userName = prompt("Ismingizni kiriting!");
+
+
+var userAge =  prompt("Yoshingizni kiriting!");
+
+
+var userAddress =  prompt("Manzilingizni kiriting!");
+
+var userAmbition =  prompt("Saytdan foydalanishni hohlaysizmi!");
+
+console.log(userName + "  " + userAge + "  " + userAddress + "  " + userAmbition);
+
+
+
+btn.addEventListener('click', () => {
+    var year = prompt("Enter your birth year");
+    var current = 2022 - year;
+    console.log(date);
+})
